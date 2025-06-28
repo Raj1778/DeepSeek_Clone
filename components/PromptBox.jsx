08403a67a453ecada77d2a9636e71ede8556ffd4
Raj1778 <164516744+Raj1778@users.noreply.github.com>
2025-06-28 18:08:21 +0530

    Added Prompt Box

--- components/PromptBox.jsx
+++ components/PromptBox.jsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { assets } from "@/assets/assets";
+import React, { useState } from "react";
+import Image from "next/image";
 
-const PromptBox = () => {
+const PromptBox = ({ isLoading, setIsLoading }) => {
+  const [prompt, setPrompt] = useState("");
   return (
     <form
       className={`w-full ${false ? "max-w-3xl" : "max-w-2xl"}
@@ -11,7 +14,40 @@ const PromptBox = () => {
         rows={2}
         placeholder="Message DeepSeek"
         required
+        onChange={(e) => setPrompt(e.target.value)}
+        value={prompt}
       ></textarea>
+
+      <div className="flex items-center justify-between text-sm">
+        <div className="flex items-center gap-2">
+          <p className="flex items-center gap-2 text-xs border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition">
+            <Image className="h-5" src={assets.deepthink_icon} alt=""></Image>
+            DeepThink (R1)
+          </p>
+          <p className="flex items-center gap-2 text-xs border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition">
+            <Image className="h-5" src={assets.search_icon} alt=""></Image>
+            Search
+          </p>
+        </div>
+        <div>
+          <Image
+            className="w-4 cursor-pointer"
+            src={assets.pin_icon}
+            alt=""
+          ></Image>
+          <button
+            className={`${
+              prompt ? "bg-primary" : "bg-[#71717a]"
+            } rounded-full p-2 cursor-pointer`}
+          >
+            <Image
+              className="w-3.5 aspect-square"
+              src={prompt ? assets.arrow_icon : assets.arrow_icon_dull}
+              alt=""
+            ></Image>
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
